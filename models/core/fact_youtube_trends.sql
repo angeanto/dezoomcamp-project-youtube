0a969e3c2f5200ae418994963d216604a4f5b6d4
Anto Ange <roguesbest@hotmail.com>
2023-03-18 17:48:08 +0000

    Cluster and partition

--- models/core/fact_youtube_trends.sql
+++ models/core/fact_youtube_trends.sql
@@ -1,8 +1,12 @@
-{{ config(materialized='table') }}
+{{ config(materialized='table',
+          partition_by = {"field": "trending_date_timestamp","data_type": "timestamp","granularity": "day"},
+          cluster_by = "categoryId"
+          )}}
 
 with youtube_data as (
-    select *
-    from {{ ref('stg_youtube_trends.sql') }}
+    select *,
+    cast(trending_date as timestamp) AS trending_date_timestamp
+    from {{ ref('stg_youtube_trends') }}
 ) 
 select 
 *
